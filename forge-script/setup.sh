8702f464e7f44c342d4526b7c33cd3771a6fc64b
yohanelly95 <yohan.nelson@gmail.com>
2024-07-23 13:26:17 +0530

    feat: save deployed addresses

--- forge-script/setup.sh
+++ forge-script/setup.sh
@@ -25,24 +25,27 @@ else
    exit 1
 fi
 
+# Ensure the .deployed-contract-addresses file exists and truncate it to make sure it is empty
+touch .deployed-contract-addresses
+: > .deployed-contract-addresses
 
 echo "[LOG] Deploying SigVerifyLib..."
 SIGVERIFY_LIB_OUTPUT=$(forge script $DEPLOY_SCRIPT --sig "deploySigVerifyLib()" $FORGE_COMMAND_SUFFIX | grep LOG)
 export SIGVERIFY_LIB_ADDRESS=$(echo $SIGVERIFY_LIB_OUTPUT | grep -oE '0x[0-9A-Fa-f]+')
-echo "SIGVERIFY_LIB_ADDRESS=$SIGVERIFY_LIB_ADDRESS" >> .env
+echo "SIGVERIFY_LIB_ADDRESS=$SIGVERIFY_LIB_ADDRESS" >> .deployed-contract-addresses
 # sed -i "" "${STARTING_LINE}i\\$SIGVERIFY_LIB_ADDRESS" .env
 echo $SIGVERIFY_LIB_OUTPUT
 
 echo "[LOG] Deploying PEMCertChainLib..."
 PEMCERT_LIB_OUTPUT=$(forge script $DEPLOY_SCRIPT --sig "deployPemCertLib()" $FORGE_COMMAND_SUFFIX | grep LOG)
 export PEMCERT_LIB_ADDRESS=$(echo $PEMCERT_LIB_OUTPUT | grep -oE '0x[0-9A-Fa-f]+')
-echo "PEMCERT_LIB_ADDRESS=$PEMCERT_LIB_ADDRESS" >> .env
+echo "PEMCERT_LIB_ADDRESS=$PEMCERT_LIB_ADDRESS" >> .deployed-contract-addresses
 echo $PEMCERT_LIB_OUTPUT
 
 echo "[LOG] Deploying AutomataDcapV3Attestation..."
 DCAP_ATTESTATION_OUTPUT=$(forge script $DEPLOY_SCRIPT --sig "deployAttestation()" $FORGE_COMMAND_SUFFIX | grep LOG)
 export DCAP_ATTESTATION_ADDRESS=$(echo $DCAP_ATTESTATION_OUTPUT | grep -oE '0x[0-9A-Fa-f]+')
-echo "DCAP_ATTESTATION_ADDRESS=$DCAP_ATTESTATION_ADDRESS" >> .env
+echo "DCAP_ATTESTATION_ADDRESS=$DCAP_ATTESTATION_ADDRESS" >> .deployed-contract-addresses
 echo $DCAP_ATTESTATION_OUTPUT
 
 echo "[LOG] Contract Deployment is complete. Setting up the attestation contract..."
