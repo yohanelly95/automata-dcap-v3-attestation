2f666d651fb034522f1bff74a2de200a6534c3b1
yohanelly95 <yohan.nelson@gmail.com>
2024-07-29 14:08:47 +0530

    chore: stop monitor script

--- stop-monitor.sh
+++ stop-monitor.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# Find the PID of monitor.sh
-PID=$(ps aux | grep 'monitor.sh' | awk '{print $2}')
-echo "PID=$PID"
-# Check if PID is not empty
-if [ -n "$PID" ]; then
-  echo "Stopping monitor.sh with PID $PID"
-  kill $PID
-  # If the process does not stop, force kill it
-  sleep 2
-  if ps -p $PID > /dev/null; then
-    echo "Force killing monitor.sh with PID $PID"
-    kill -9 $PID
-  fi
+# Check for running anvil processes
+PIDS=$(pgrep -f anvil)
+
+# Check if PIDS is not empty
+if [ -n "$PIDS" ]; then
+  echo "Stopping anvil with PIDs: $PIDS"
+  for PID in $PIDS; do
+    echo "Killing PID $PID"
+    kill $PID
+    # If the process does not stop, force kill it
+    sleep 2
+    if ps -p $PID > /dev/null; then
+      echo "Force killing PID $PID"
+      kill -9 $PID
+    fi
+  done
 else
-  echo "monitor.sh is not running."
+  echo "anvil is not running."
 fi
